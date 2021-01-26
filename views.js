34a5a1bde63516025f00d46d25b76f9014fb29e1
rasdinho <ras.dinho7@gmail.com>
2021-01-26 16:53:10 -0500

    js-x-y-speed

--- views.js
+++ views.js
@@ -6,3 +6,14 @@ let axisTreshold = 30;
 let distanceLimit = 70;
 let mouseX;
 let mouseY;
+let xForward = true;
+let yForward = true;
+let refreshBase = 2;
+let refreshDelay = refreshBase;
+let refreshDelta = 55;
+let timer = false;
+let speedPercentage = 0;
+const speedFactor = 0.8;
+let navigatorActive = false;
+
+let aspectRatio = "landscape";
