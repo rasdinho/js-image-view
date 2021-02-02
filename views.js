1fd52b1e5dc04f8587f3a1a46433ef8df1cd2349
rasdinho <ras.dinho7@gmail.com>
2021-02-02 17:24:29 -0500

    element-offset

--- views.js
+++ views.js
@@ -122,3 +122,14 @@ const calculateDistance = (elem, mouseX, mouseY) => {
     )
   );
 };
+// Get element offsets
+const getOffset = el => {
+  var _x = 0;
+  var _y = 0;
+  while (el && !isNaN(el.offsetLeft) && !isNaN(el.offsetTop)) {
+    _x += el.offsetLeft - el.scrollLeft;
+    _y += el.offsetTop - el.scrollTop;
+    el = el.offsetParent;
+  }
+  return { top: _y, left: _x };
+};
